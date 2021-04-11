2c0c9c8b9501b736036eb057f1a35f0e43e3911e
amithcali <amith@calibrage.in>
2021-04-11 19:34:34 -0400

    rebase example

--- app/src/main/java/com/tim/burtons/ui/products/ProductsViewModel.kt
+++ app/src/main/java/com/tim/burtons/ui/products/ProductsViewModel.kt
@@ -14,6 +14,7 @@ class ProductsViewModel @ViewModelInject constructor(private val productsReposit
 
     /* add a branch */
     /* new line is added in the master */
+    /* rebase practice */
     init {
         getProducts()
     }
