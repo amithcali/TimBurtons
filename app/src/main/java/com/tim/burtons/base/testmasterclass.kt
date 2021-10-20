66eb4dfe3dbcba626c737a6ff2335d2495f1e1b5
amithcali <amith@calibrage.in>
2021-10-20 18:44:43 -0400

    remove class

--- app/src/main/java/com/tim/burtons/base/testmasterclass.kt
+++
@@ -1,9 +0,0 @@
-package com.tim.burtons.base
-
-import android.os.Parcel
-import android.os.Parcelable
-import android.view.View
-import androidx.lifecycle.ViewModel
-
-class testmasterclass : ViewModel() {
-}
