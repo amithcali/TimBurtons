f66b15d02bf9b6b8b45b10a5c815cb669ac1cb06
amithcali <amith@calibrage.in>
2021-10-20 18:24:46 -0400

    test master branch

---
+++ app/src/main/java/com/tim/burtons/base/testmasterclass.kt
@@ -0,0 +1,9 @@
+package com.tim.burtons.base
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class testmasterclass : ViewModel() {
+}
